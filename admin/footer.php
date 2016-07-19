84dd692ca2cb71c3b735e22666867c34fdb743de
Jack <1069072177@qq.com>
2016-07-19 20:54:48 +0800

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,7 @@
+
+
+	<footer id='footer'>
+		<div>This is footer</div>
+	</footer><!-- .site-footer -->
+</body>
+</html>
---
+++ admin/header.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CDTF SAMS</title>
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<!-- <script src="js/jquery.min.js"></script> -->
+<script src="js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="css/sams.css">
+
+</head>
+
+<body>
+<p>This is header</p>
+
+
