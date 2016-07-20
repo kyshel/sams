7d3baa701778e3e845c90087304790b5ca84c6e6
Jack <1069072177@qq.com>
2016-07-20 21:42:34 +0800

    add navbar

--- admin/header.php
+++ admin/header.php
@@ -5,7 +5,7 @@
 
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-<!-- <script src="js/jquery.min.js"></script> -->
+<script src="js/jquery-3.1.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" href="css/sams.css">
@@ -13,6 +13,27 @@
 </head>
 
 <body>
-<p>This is header</p>
 
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+  
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">CDTF SAMS</a>
+    </div>
+
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+
+
+        <li><a href="">Logout</a></li>
+    </div>
+
+  </div>
+</nav>
 
